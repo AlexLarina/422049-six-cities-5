da9a8dc039f9bfbf7cb0acd124efcf29da7356de
Alexandra Larina <aa.larina.ff@gmail.com>
2020-10-05 01:00:52 +0300

    Adding main page and app components. Adding props and its validation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app";
 
+const Settings = {
+  RENT_AMOUNT: 312
+};
+
+ReactDOM.render(
+    <App
+      rentItemsAmount = {Settings.RENT_AMOUNT}
+    />,
+    document.querySelector(`#root`)
+);
