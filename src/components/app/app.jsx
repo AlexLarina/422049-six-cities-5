78a3c527d0f3309d93fa7ef90a7f53f3c795023f
Alexandra Larina <aa.larina.ff@gmail.com>
2020-10-05 18:40:33 +0300

    Adding router

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
-import MainScreen from "../main-screen/main-screen.jsx";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+
+import MainScreen from "../main/main-screen.jsx";
+import LoginScreen from "../login/login-screen.jsx";
+import FavoritesScreen from "../favorites/favorites-screen.jsx";
+import OfferScreen from "../offer/offer-screen.jsx";
 
 const App = (props) => {
 
   const {rentItemsAmount} = props;
 
   return (
-    <MainScreen rentItemsAmount={rentItemsAmount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen rentItemsAmount={rentItemsAmount} />
+        </Route>
+
+        <Route exact path="/login">
+          <LoginScreen />
+        </Route>
+
+        <Route exact path="/favorites">
+          <FavoritesScreen />
+        </Route>
+
+        <Route exact path="/offer">
+          <OfferScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
