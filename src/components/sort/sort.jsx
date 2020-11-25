d97f66483732e36dc3b99fca8e652b6cfbc48c1a
Alexandra Larina <aa.larina.ff@gmail.com>
2020-11-25 12:28:40 +0300

    Fixing closing of sort option block after change

--- src/components/sort/sort.jsx
+++ src/components/sort/sort.jsx
@@ -8,26 +8,35 @@ class Sort extends PureComponent {
     super(props);
 
     this._handleClick = this._handleClick.bind(this);
-    this._sortOptionsRef = React.createRef();
+    this._sortOptionsListRef = React.createRef();
+    this._currentSortOptionRef = React.createRef();
+
+    this._currentSortOption = `Popular`;
   }
 
-  _handleClick() {
-    this._sortOptionsRef.current.classList.toggle(`places__options--opened`);
+  _handleClick(evt) {
+    this._sortOptionsListRef.current.classList.toggle(`places__options--opened`);
+    // @TO-DO как бы мне накидывать на выбранный option класс active, чтобы миллион ссылок не городить
+
+    if (evt.target.textContent !== this._currentSortOption) {
+      this.props.handleSortClick(evt);
+      this._currentSortOption = evt.target.textContent;
+      // @TO-DO как бы тут всобачить innerHtml, чтобы svg иконка не пропадала
+      this._currentSortOptionRef.current.textContent = this._currentSortOption;
+    }
   }
 
   render() {
-    const {handleSortClick} = this.props;
-
     return (
       <form className="places__sorting" action="#" method="get">
         <span className="places__sorting-caption">Sort by</span>
-        <span className="places__sorting-type" tabIndex="0" onClick={this._handleClick}>
-          Popular
+        <span ref={this._currentSortOptionRef} className="places__sorting-type" tabIndex="0" onClick={this._handleClick}>
+          {this._currentSortOption}
           <svg className="places__sorting-arrow" width="7" height="4">
             <use xlinkHref="#icon-arrow-select"></use>
           </svg>
         </span>
-        <ul ref={this._sortOptionsRef} onClick={handleSortClick} className="places__options places__options--custom">
+        <ul ref={this._sortOptionsListRef} onClick={this._handleClick} className="places__options places__options--custom">
           <li className="places__option places__option--active" tabIndex="0">Popular</li>
           <li className="places__option" tabIndex="0">Price: low to high</li>
           <li className="places__option" tabIndex="0">Price: high to low</li>
